c506f9f3642b47b31557270ecc8a6cb00a351870
VOpoka <144518288+VOpoka@users.noreply.github.com>
2023-09-16 17:44:40 +0200

    Added the code

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ FPS=pygame.time.Clock()
 COLOR_WHITE=(255, 255, 255)
 COLOR_BLACK=(0, 0, 0)
 COLOR_BLUE=(0, 0, 255)
+COLOR_GREEN=(0, 255, 0)
 
 main_display=pygame.display.set_mode((WIDTH, HEIGTH))
 
 player_size=(20, 20)
-player=pygame.Surface((player_size))
+player=pygame.Surface(player_size)
 player.fill((COLOR_WHITE))
 player_rect=player.get_rect()
 #player_speed=[1, 1]
@@ -33,20 +34,33 @@ def create_enemy():
     enemy_rect=pygame.Rect(WIDTH, random.randint(0, HEIGTH), *enemy_size)
     enemy_move=[random.randint(-6, -1), 0]
     return [enemy, enemy_rect, enemy_move]
+
 CREATE_ENEMY=pygame.USEREVENT+1
 pygame.time.set_timer(CREATE_ENEMY, 1500)
 enemies=[]
+def create_prize():
+    prize_size=(35, 35)
+    prize=pygame.Surface(prize_size)
+    prize.fill(COLOR_GREEN)
+    prize_rect=pygame.Rect(random.randint(0, WIDTH), 0, *prize_size)
+    prize_move=[0, random.randint(1,6)]
+    return [prize, prize_rect, prize_move]
 
+CREATE_PRIZE=pygame.USEREVENT+2
+pygame.time.set_timer(CREATE_PRIZE, 1900)
+prizeies=[]
 
 playing=True
-
 while playing:
-    FPS.tick(240)
+    FPS.tick(240)    
     for event in pygame.event.get():
         if event.type==QUIT:
             playing=False
         if event.type==CREATE_ENEMY:
             enemies.append(create_enemy())
+                     
+        if event.type==CREATE_PRIZE:
+            prizeies.append(create_prize())                      
     
     main_display.fill(COLOR_BLACK) 
     keys=pygame.key.get_pressed()
@@ -62,22 +76,26 @@ while playing:
         
     if keys[K_RIGHT]and player_rect.right<WIDTH:
         player_rect=player_rect.move(player_move_right)
+        
+    main_display.blit(player, player_rect)
+        
     for enemy in enemies:
         enemy[1]=enemy[1].move(enemy[2])
         main_display.blit(enemy[0], enemy[1])
         
-   # enemy_rect=enemy_rect.move(enemy_move)
-           
-    main_display.blit(player, player_rect)
-   # main_display.blit(enemy, enemy_rect)
-    
-    print(len(enemies))
-    
+    for prize in prizeies:
+        prize[1]=prize[1].move(prize[2])
+        main_display.blit(prize[0], prize[1])
+          
     pygame.display.flip() 
     
     for enemy in enemies:
         if enemy[1].left<0:
             enemies.pop(enemies.index(enemy))
+            
+    for prize in prizeies:
+        if prize[1].bottom<0:
+            prizeies.pop(prizeies.index(prize))
         
          
         
